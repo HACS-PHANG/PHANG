235fa7e8a89b3c8a986b9c52e01f895b6dc1f5a9
Pallavi <pallum9@gmail.com>
2022-10-02 19:51:23 +0000

    Added script calls in container recycling

--- scripts/container_recycling.sh
+++ scripts/container_recycling.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Initializing this now as an argument but might need to update
+# Initializing these now as arguments but might need to update
 container_name=$1
+aws_ip=$2
+
+# remove attacker
+./remove_attacker $container_name
 
 # cleanup + data gathering off of old container
 ./data_upload $container_name
@@ -19,6 +23,8 @@ lxc-destroy -n $container_name
 ./command_poisoning $container_name
 
 # set up mitm
+./iptables_recycle_untested $container_name $aws_ip
 
 # set up banner + honey
 ./banner $container_name
+./honey_set_up $container_name
