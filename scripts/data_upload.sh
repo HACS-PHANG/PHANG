3730aad6c3063e8506ce14e8577b488861ebaa8b
Pallavi <pallum9@gmail.com>
2022-10-04 18:40:32 +0000

    Updated data_upload to only upload logs + downloads

--- scripts/data_upload.sh
+++ scripts/data_upload.sh
@@ -1,21 +1,18 @@
 #!/bin/bash -e
 name=$1
 
-# Checkout branch meant for all data collected (so that we don't clutter main)
-git checkout data_collection
-
-# Move into the log_files directory
-cd log_files
+# Move into the honeypot_files directory
+cd honeypot_files
 
 # Copy all files in secret folder (all files that are downloaded by attacker
-#and stored in this folder by command poisoning script) into local folder
-#to be analyzed by virustotal
-cp /var/lib/lxc/$name/rootfs/var/log/.downloads ~/containerdownloads/$name
+# and stored in this folder by command poisoning script) into git repo
+# to be analyzed by virustotal
+cp /var/lib/lxc/$name/rootfs/var/log/.downloads ./$name/containerdownloads/
+cp /home/student/MITM/$name.log ./$name/mitmlogs/
+cp /var/lib/lxc/$name/var/log/auth.log ./$name/authlogs/
 
-# Copy all the files from the lxc container into current location in git repo
-cp /var/lib/lxc/$name .
 
 # Add, commit, and push all changes to github
-git add .
-git commit -m “Uploading log files for $name”
+git add ./$name
+git commit -m “Uploading downloaded files for $name”
 git push
