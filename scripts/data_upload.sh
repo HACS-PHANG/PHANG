e1fcb0874afc57d2d4cad859525c62d83e389a0f
Pallavi <pallum9@gmail.com>
2022-10-04 18:43:47 +0000

    cleaned up data_upload a little

--- scripts/data_upload.sh
+++ scripts/data_upload.sh
@@ -2,17 +2,17 @@
 name=$1
 
 # Move into the honeypot_files directory
-cd honeypot_files
+cd honeypot_files/$name
 
 # Copy all files in secret folder (all files that are downloaded by attacker
 # and stored in this folder by command poisoning script) into git repo
 # to be analyzed by virustotal
-cp /var/lib/lxc/$name/rootfs/var/log/.downloads ./$name/containerdownloads/
-cp /home/student/MITM/$name.log ./$name/mitmlogs/
-cp /var/lib/lxc/$name/var/log/auth.log ./$name/authlogs/
+cp /var/lib/lxc/$name/rootfs/var/log/.downloads ./containerdownloads/
+cp /home/student/MITM/$name.log ./mitmlogs/
+cp /var/lib/lxc/$name/var/log/auth.log ./authlogs/
 
 
 # Add, commit, and push all changes to github
-git add ./$name
+git add .
 git commit -m “Uploading downloaded files for $name”
 git push
