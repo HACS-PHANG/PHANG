534599e916e1c5862f2e28b99aec296a232f6af3
Grace <gracek7689@gmail.com>
2022-10-08 18:19:41 -0400

    Adding script to count the number of malicious downloads and total number of downloads

---
+++ VirusTotalUploaderProgram.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+#whoever hosts the containers should have already installed boto3 and python3
+# python -m pip install boto3
+
+cd /home/student 
+
+#clone virus total uploader repository
+git clone https://github.com/gitgiant/VT_Uploader.git 
+cd VT_Uploader
+
+#insert the VirusTotal API Key
+echo "key = \"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\"" > tokens.py
+
+#insert the region.
+sed -i "s|raise NoRegionError()|region_name = 'us-east-2'|g" /home/student/.local/lib/python3.8/site-packages/botocore/regions.py
+
+# sub out the is with == so it doesn't throw an error.
+sed -i "s|if (platform.system()) is 'Windows':|if (platform.system()) == 'Windows':|g" /home/student/VT_Uploader/main.py
+
+
+#command to run the python program
+python3 main.py -d /home/student/containerdownloads/$name
