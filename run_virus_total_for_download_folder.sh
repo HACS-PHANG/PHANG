8d0b7f30eee60397cbb11a791e77b2346a5b287a
Grace <gracek7689@gmail.com>
2022-10-08 19:13:33 -0400

    Script to run all downloads in the container directory through VirusTotal

--- run_virus_total_for_download_folder.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#command to run the python program
-python3 main.py -d /home/student/containerdownloads/$name
---
+++ scripts/run_virus_total_for_download_folder.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#command to run the python program
+python3 main.py -d /home/student/containerdownloads/$name/.downloads
+
