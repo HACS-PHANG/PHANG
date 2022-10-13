ade4509fd830ec381c77727b3a7669c4c5a24025
ahiba27 <67114769+ahiba27@users.noreply.github.com>
2022-10-13 11:25:38 -0400

    changed name

--- scripts/honey_set_up.sh
+++ scripts/honey_set_up.sh
@@ -1,4 +1,4 @@
 #Set up honey
 
 #!/bin/bash
-sudo cp -r ~/honey_example.csv /var/lib/lxc/"$1"/rootfs/usr/employee_records.csv
+sudo cp -r ~/employee_records /var/lib/lxc/"$1"/rootfs/usr/company_data
