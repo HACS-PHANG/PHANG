ab32c9fc126048bfeb381758d967db421c1139d1
gracek7689 <74626913+gracek7689@users.noreply.github.com>
2022-10-17 16:37:11 -0400

    insert warning message for banner

--- scripts/banner.sh
+++ scripts/banner.sh
@@ -6,4 +6,4 @@ then
 fi
 
 #edit motd to display warning message upon login
-sudo lxc-attach -n $1 -- bash -c "echo -e 'message' > /etc/motd"
+sudo lxc-attach -n $1 -- bash -c "echo -e '\n\e[31m========================================\n\n WARNING: You are being monitored\n\n========================================\n\e[0m' > /etc/motd"
