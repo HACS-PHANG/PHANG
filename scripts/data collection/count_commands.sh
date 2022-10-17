df30c3088694a4d17fd2c4faab0e6594d25d2c46
gracek7689 <74626913+gracek7689@users.noreply.github.com>
2022-10-17 17:45:04 -0400

    Update count_commands.sh

--- scripts/data collection/count_commands.sh
+++ scripts/data collection/count_commands.sh
@@ -1,12 +1,15 @@
 #!/bin/bash 
-
+name=$1
+bannertype=$2
+filepathtosearch = "/home/student/$bannertype/$name/$name.log"
+filepathtooutput = "/home/student/$bannertype/$name/count_commands"
 echo "Pure Information Gathering:" >> /home/student/container_reports/$name
-echo "number of ls commands: $("grep -c ls /home/mitm/session_logs/$name") >> /home/student/container_reports/$name
-echo "number of cd commands: $("grep -c cd /home/mitm/session_logs/$name") >> /home/student/container_reports/$name
-echo "Surface Level Modification:" >> /home/student/container_reports/$name
-echo "number of vim commands: $("grep -c vim /home/mitm/session_logs/$name") >> /home/student/container_reports/$name
-echo "number of touch commands: $("grep -c touch /home/mitm/session_logs/$name") >> /home/student/container_reports/$name
-echo "number of curl commands: $("grep -c curl /home/mitm/session_logs/$name") >> /home/student/container_reports/$name
-echo "number of wget commands: $("grep -c wget /home/mitm/session_logs/$name") >> /home/student/container_reports/$name
-echo "Critical Level Modification:" >> /.containerlogoccurrences/$name
-echo "number of chmod commands: $("grep -c chmod /home/mitm/session_logs/$name") >> /home/student/container_reports/$name
+echo "number of ls commands: $("grep -c ls $filepathtosearch") >> $filepathtooutput
+echo "number of cd commands: $("grep -c cd $filepathtosearch") >> $filepathtooutput
+echo "Surface Level Modification:" >> $filepathtooutput
+echo "number of vim commands: $("grep -c vim $filepathtosearch") >> $filepathtooutput
+echo "number of touch commands: $("grep -c touch $filepathtosearch") >> $filepathtooutput
+echo "number of curl commands: $("grep -c curl $filepathtosearch") >> $filepathtooutput
+echo "number of wget commands: $("grep -c wget $filepathtosearch") >> $filepathtooutput
+echo "Critical Level Modification:" >> $filepathtooutput
+echo "number of chmod commands: $("grep -c chmod $filepathtosearch") >> $filepathtooutput
