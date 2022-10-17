a6758dd9d3208f1b6f3f1e07d588719f7d566ca8
gracek7689 <74626913+gracek7689@users.noreply.github.com>
2022-10-17 17:45:19 -0400

    Update cycle.sh

--- scripts/cycle.sh
+++ scripts/cycle.sh
@@ -3,6 +3,10 @@
 # Initializing these now as arguments but might need to update
 container_name=$1
 aws_ip=$2
+banner_type = $3 # nobanner or banner
+timestamp=$(date +%s)
+container_name = container_name + timestamp
+
 
 while :
 do
@@ -19,7 +23,10 @@ do
 ./iptables_recycle_untested $container_name $aws_ip
 
 # set up banner + honey
+if [ $banner_type == "banner" ]
+then
 ./banner $container_name
+fi
 ./honey_set_up $container_name
 
 #Tail on session log
@@ -29,6 +36,6 @@ do
 	sleep 1
 end
 sudo lxc-attach -n $name -- bash -c "(sudo crontab -l 2>/dev/null; echo '20 * * * * /usr/bin/pkill -KILL -u $user') | sudo crontab -"
-(sudo crontab -l 2>/dev/null; echo "*20 * * * * ./remove_attacker $container_name && ./data_upload $container_name && ./count_commands $container_name && sudo lxc-stop -n $container_name && sudo lxc-destroy -n $container_name") | sudo crontab -
+(sudo crontab -l 2>/dev/null; echo "*20 * * * * ./remove_attacker $container_name && ./data_upload $container_name && ./count_commands $container_name $banner_type && sudo lxc-stop -n $container_name && sudo lxc-destroy -n $container_name") | sudo crontab -
 
 end
