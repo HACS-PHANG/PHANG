4e26784f9d565f4e49613ac48e68ff1669e7cf16
Pallavi <pallum9@gmail.com>
2022-10-18 05:00:31 +0000

    minor changes + added clean-up to cycle.sh

--- scripts/cycle.sh
+++ scripts/cycle.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+
+# Checking args first
+if [[ $# -ne 4 ]]
+then
+    echo "./cycle.sh [container name] [external ip] [banner type] [port]"
+    exit 1
+fi
+
 # Initializing these now as arguments but might need to update
-container_name=$1
+container_name_init=$1
 ext_ip=$2
 banner_type=$3 # nobanner or banner
+port=$4
 timestamp=$(date +%s)
-container_name="${container_name}${timestamp}"
+container_name="${container_name_init}${timestamp}"
 ips_file="ext_ip.txt"
-#cd /home/student/PHANG/scripts
-
-
-
-# we need to add deleting container and data upload
-
+#cd /home/student/PHANG/scripts <-- need to figure out
 
 # create new container
 mkdir /home/student/$banner_type/$container_name
@@ -28,7 +32,7 @@ echo "=============================================== end of ssh setup"
 echo "============================================end of command poisoning"
 
 # set up mitm
-./working_iptables.sh $container_name $ext_ip 12345 $banner_type
+./working_iptables.sh $container_name $ext_ip $port $banner_type
 echo "================================================= end of working iptables"
 
 
@@ -50,4 +54,8 @@ echo "Success"
 #sudo lxc-attach -n $name -- bash -c "(sudo crontab -l 2>/dev/null; echo '20 * * * * /usr/bin/pkill -KILL -u $user') | sudo crontab -"
 #(sudo crontab -l 2>/dev/null; echo "*20 * * * * ./remove_attacker $container_name && ./data_upload $container_name && ./count_commands $container_name $banner_type && sudo lxc-stop -n $container_name && sudo lxc-destroy -n $container_name && ./cycle.sh $name 128.8.238.32 $banner_type") | sudo crontab -e
 
-
+# Cleaning up iptables + container <-- need to move based on what's calling cycle.sh
+./remove_iptables $container_name $ext_ip $port
+sudo cp -r /var/lib/lxc/$name/rootfs/var/log/.downloads /home/student/$banner_type/$name/downloads
+sudo lxc-stop -n $container_name
+sudo lxc-destroy -n $container_name
