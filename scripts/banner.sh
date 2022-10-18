1942b9f7fe8f8e1e86a6b15846756ed7e19038a2
Pallavi <pallum9@gmail.com>
2022-10-18 20:12:57 +0000

    Adding banner messaging

--- scripts/banner.sh
+++ scripts/banner.sh
@@ -5,5 +5,15 @@ then
   exit 1
 fi
 bannertype=$2
+if [[ $bannertype == "FBI" ]]
+then
+  bannertype="by the Federal Bureau of Investigation"
+elif [[ $bannertype == "" ]]
+then
+  bannertype="by the Maryland State Police IT Division"
+elif [[ $bannertype == "" ]]
+then
+  bannertype="by the University of Maryland Department of IT"
+fi
 #edit motd to display warning message upon login
 sudo lxc-attach -n $1 -- bash -c "echo -e '\n\e[31m========================================\n\n WARNING: You are being monitored ${bannertype} \n\n========================================\n\e[0m' > /etc/motd"
