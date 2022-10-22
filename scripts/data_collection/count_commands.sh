c30e655edfc61ef9fa80415cd0775f7d96f7e606
Pallavi <pallum9@gmail.com>
2022-10-22 18:23:06 +0000

    change count commands

--- scripts/data_collection/count_commands.sh
+++ scripts/data_collection/count_commands.sh
@@ -5,15 +5,15 @@ filepathtosearch="/home/student/$bannertype/$name/$name.log"
 filepathtooutput="/home/student/$bannertype/$name/count_commands"
 echo "Pure Information Gathering:" >> $filepathtooutput
 sleep 5
-lsgrep=$(grep -c ls "${filepathtosearch}")
-cdgrep=$(grep -c cd "${filepathtosearch}")
+lsgrep=$(grep -w -o ls "${filepathtosearch}" | wc -l)
+cdgrep=$(grep -w -o cd "${filepathtosearch}" | wc -l)
 
-vimgrep=$(grep -c vim "${filepathtosearch}")
-touchgrep=$(grep -c touch "${filepathtosearch}")
-curlgrep=$(grep -c curl "${filepathtosearch}")
-wgetgrep=$(grep -c wget "${filepathtosearch}")
+vimgrep=$(grep -w -o vim "${filepathtosearch}" | wc -l)
+touchgrep=$(grep -w -o touch "${filepathtosearch}" | wc -l)
+curlgrep=$(grep -w -o curl "${filepathtosearch}" | wc -l)
+wgetgrep=$(grep -w -o wget "${filepathtosearch}" | wc -l)
 
-chmodgrep=$(grep -c chmod "${filepathtosearch}")
+chmodgrep=$(grep -w -o chmod "${filepathtosearch}" | wc -l)
 
 echo "number of ls commands: ${lsgrep}" >> "$filepathtooutput"
 echo "number of cd commands: ${cdgrep}" >> "$filepathtooutput"
